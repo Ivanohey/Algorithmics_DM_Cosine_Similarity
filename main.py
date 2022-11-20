c7948f3d34e07135c4d4f74c188cb3bd9ff119b7
Ivan Kostine <ivan.kostine@ibm.com>
2022-11-20 22:22:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+#Define variables
+filename = "./ressources/ref-sentences.txt"
+
+
+#Reading lines and storing them in a list
+def read_reference_text(filename):
+    f = open(filename)
+    for line in f:
+        print(line)
+    f.close()
+    #sentence_list = []
+    #return
+
+#
+def make_word_vector(w, list):
+    word_dictionary = ""
+    return word_dictionary
+
+#We execute the file
+read_reference_text(filename)
